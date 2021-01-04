c752925799c4076d2ec32d85bff8c986d2ee3c06
ArthurGalerneau <arthur.galerneau@gmail.com>
2021-01-04 22:25:40 +0100

    avancement sur le websocket scala

--- websocket_scala-akka/projetv0/src/main/scala/mainapi.scala
+++ websocket_scala-akka/projetv0/src/main/scala/mainapi.scala
@@ -2,7 +2,10 @@ package docs.http.scaladsl
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.{Done, NotUsed}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -26,7 +29,34 @@ object mainapi {
 
   def main(args: Array[String]): Unit = {
 
-    sendRequest().foreach(println)
+    //sendRequest().foreach(println)
+    implicit val system = ActorSystem()
+
+    val printSink: Sink[Message, Future[Done]] =
+      Sink.foreach {
+        case message: TextMessage.Strict =>
+          println(message.text)
+        case _ =>
+      }
+
+    val helloSource: Source[Message, NotUsed] =
+      Source.single(TextMessage.Strict("""{"type": "subscribe", "symbol": "BINANCE:BTCUSDT"}"""))
+
+    val flow: Flow[Message, Message, Future[Done]] =
+      Flow.fromSinkAndSourceMat(printSink, helloSource)(Keep.left)
+
+    val (upgradeResponse, closed) =
+      Http().singleWebSocketRequest(WebSocketRequest("wss://ws.finnhub.io?token=bv63gcn48v6phr4c7370"), flow)
+
+    val connected = upgradeResponse.map { upgrade =>
+      if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
+        Done
+      } else {
+        throw new RuntimeException(s"Connection failed: ${upgrade.response.status}")
+      }
+    }
+    connected.onComplete(println)
+    closed.foreach(_ => println("closed"))
 
   }
 
